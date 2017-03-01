c1b529a8ad99ff5e092554e3aae8662657b6e744
randhyllcho <randhyllcho@gmail.com>
2017-03-01 15:57:02 -0800

    final clean and added exploratory vis

--- epa.csv/EVs_2017.R
+++ epa.csv/EVs_2017.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggrepel)
 
 EV <- read.csv("EVs.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"))
 str(EV)
@@ -13,11 +14,16 @@ newNames <- c("Make", "Range", "Model", "Drive", "Class", "Watt-Hr/Kg")
 colnames(EV) <- newNames
 
 EV <- EV %>% mutate_at(vars(Range, `Watt-Hr/Kg`), funs(as.numeric))
-EV %>% separate(col = Drive, into = "Drive", sep = "-1")
+# EV %>% separate(col = Drive, into = "Drive", sep = "-1")
 
 EV$Drive[grepl("Front",EV$Drive)] <- "Front"
 EV$Drive[grepl("Rear", EV$Drive)] <- "Rear"
 EV$Drive[grepl("All", EV$Drive)] <- "AWD"
 EV <- EV %>% mutate_at(vars(Drive), funs(as.factor))
-models <- EV %>% distinct(Model)
+EV <- EV %>% distinct(Model, .keep_all = TRUE)
+
+ggplot(EV, aes(Make, Range)) +
+  geom_label_repel(aes(label = paste(Model, Range, sep = ": ")), color = "dodgerblue") +
+  theme_classic()
+
 
